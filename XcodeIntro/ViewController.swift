2f38b4c5ecc2615ec5086e3ae022a0fa7d235f36
Elin <elin.simonsson@outlook.com>
2022-11-29 16:33:24 +0100

    first commit

--- XcodeIntro/ViewController.swift
+++ XcodeIntro/ViewController.swift
@@ -9,11 +9,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var counterLabel: UILabel!
+    
+    var counter = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        //counterLabel.text = "Elin"
+       
+        
+        
+        
     }
 
 
+    @IBAction func buttonPressed(_ sender: UIButton) {
+       // print("nu trycks knappen")
+        
+        counter += 1
+        counterLabel.text = String(counter)
+    }
 }
 
